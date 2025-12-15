e5a605eb9681fd9676239f70181029b17ff39766
DA-GEN <dyadya.dagen@gmail.com>
2025-12-15 16:35:37 +0200

    lafixed bugs

--- lab03.collections/tasks.py
+++ lab03.collections/tasks.py
@@ -1,9 +1,13 @@
-def task1(l):    
-    print(f'Length of the list: {len(l)}')
-    print(f'Middle of the list: {(sum(l)/2)}')
-    print(f'Reversed list: {l[::-1]}')
-    
-    print(' ')
+def task1(words_list):
+    if words_list:
+        average = sum(words_list) / len(words_list) 
+        
+        print(f'Length of the list: {len(words_list)}')
+        print(f'Middle of the list: {average}')
+        print(f'Reversed list: {words_list[::-1]}')
+    else:
+        print('Error: list is empty. Please try again.')
+        return task1(list(map(int, input("Enter num list: ").split())))
 
 def task2(key):
     menu = {
@@ -11,27 +15,48 @@ def task2(key):
         "banana": 5,
         "orange": 8
     }
-
-    print(menu[key])
-
-def task3(n):
     
-    dict = {}
-    for i in n:
-        if i in dict:
-            dict[i.strip('.,!?"')] += 1
-        else:
-            dict[i.strip('.,!?"')] = 1
-    
-    for key, value in dict.items():
-        print(f"{key} - {value}")
+    if key in menu: 
+        print(menu[key])
+    else:
+        print('Error: wrong element. Please try again.')
+        print("Available keys:", *menu.keys()) 
+        return task2(str(input("Enter the key: ")))
 
-    
-print('-----Task 1-----')
-task1(list(map(int, input("Enter num list: ").split())))
+def task3(words_list):
+    if words_list:
+        word_counts = {}
+        PUNCTUATION = '.,!?"'
+        
+        for raw_word in words_list:
+            clean_word = raw_word.strip(PUNCTUATION).lower() 
+            
+            if clean_word:
+                word_counts[clean_word] = word_counts.get(clean_word, 0) + 1
+        
+        print("\n--- Word Counts ---")
+        for key, value in word_counts.items():
+            print(f"{key} - {value}")
+            
+    else:
+        print("The sentence is empty. Please try again.")
+        return task3(input('Enter a sentence: ').lower().split())
 
-print('-----Task 2-----')
-task2(str(input("Enter the key: ")))
+def main():
+    print('\n-----Task 1-----')
+    try:
+        input_list = list(map(int, input("Enter num list: ").split()))
+    except ValueError:
+        print("Invalid input: Please enter only numbers separated by spaces.")
+        task1(list(map(int, input("Enter num list: ").split())))
+        
+    task1(input_list)
 
-print('-----Task 3-----')
-task3(list(map(str,input('Enter a sentence: ').lower().split())))
+    print('\n-----Task 2-----')
+    task2(input("Enter the key: ").strip().lower())
+
+    print('\n-----Task 3-----')
+    task3(input('Enter a sentence: ').lower().split())
+
+if __name__ == "__main__":
+    main()
